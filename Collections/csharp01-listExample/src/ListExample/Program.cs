e200707014e748f87a23041b9076004d088f1b5b
javi9400 <javi_mayorga94@hotmail.com>
2020-03-25 14:48:38 -0600

    inserting and removing

--- Collections/csharp01-listExample/src/ListExample/Program.cs
+++ Collections/csharp01-listExample/src/ListExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ListExample.commons;
+using ListExample.model;
 
 namespace ListExample
 {
@@ -7,11 +8,29 @@ namespace ListExample
     {
         static void Main(string[] args)
         {
-            const string PATH=@"YOUR PATH"; // copy path of file countries_stats.csv located in the source folder and paste it here
+            const string PATH=@"FILE PATH"; // copy path of file countries_stats.csv located in the source folder and paste it here
 
             CsvReader reader= new CsvReader(PATH);
             
             var countriesResult=reader.readAllCountries();
+           
+            /*
+               Adding a new country to our list
+            */
+            Country fictionalCountry= new Country("Mustafar","MT","A",1);
+
+            int fictionalCountryIndex=countriesResult.FindIndex(x=>x.Population<2000000);
+
+            /*
+              Inserts the new country
+            */
+            countriesResult.Insert(fictionalCountryIndex,fictionalCountry);
+
+           /* 
+             Removing by index
+           */
+            countriesResult.RemoveAt(fictionalCountryIndex);
+
 
             foreach (var country in countriesResult)
             {
